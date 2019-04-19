418ecbd0fd8cfc4f5cc40083eed91e3c3e4a5613
Alberto <albertocabuya2@github.com>
2019-04-19 13:46:49 -0500

    ver4

--- Bank/BankOfficer.py
+++ Bank/BankOfficer.py
@@ -11,7 +11,24 @@ class BankOfficer(QMainWindow):
     def __init__(self):
     	super(BankOfficer,self).__init__(None)
     	loadUi('bankofficial.ui',self)
+    	self.clientes= []
     	pass
+    def create_client(self,name1,name2,id,age,contra):
+    	self.cliente= [name1,name2,id,age,contra]
+    	self.cuentas= []
+    	self.cliente.append(cuentas)
+    	self.clientes.append(cliente)
+    	return "Cliente creado"
+    def update_client(self,name1,name2,id,age,contra):
+    	self.cliente= [name1,name2,id,age,contra]
+    	self.status = cliente in self.clientes
+    	if self.status == True:
+            	self.posid=self.clientes.index(cliente)
+            	self.clientes[posid]=[name1,name2,id,age,contra]
+            	return "Exito"
+            	pass
+    	else: 
+            	return "Cliente no encontrado"
 app= QApplication(sys.argv)
 ui=BankOfficer()
 ui.setWindowTitle('Test')
